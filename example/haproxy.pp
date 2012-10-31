a3126c78ec878e95438035158324852bd2df2fb0
yuxcer <yuxcer@126.com>
2012-10-31 15:49:39 +0800

    Add more example

---
+++ example/haproxy.pp
@@ -0,0 +1,1 @@
+class {'nozzle::haproxy':}
---
+++ example/nozzle_example.pp
@@ -0,0 +1,31 @@
+$api_auth_host          = '127.0.0.1'
+$api_auth_port          = '35357'
+$api_auth_protocol      = 'http'
+$admin_tenant_name  = 'service'
+$admin_user         = 'nozzle'
+$admin_password     = 'nova'
+$listen                   = '127.0.0.1'
+$listen_port_range        = '10000,61000'
+$configuration_backup_dir = '/tmp/nozzle'
+
+
+node  nozzle{
+	class  {'nozzle':}
+
+    class {'nozzle::api':
+		auth_host          =>  $api_auth_host,
+        auth_port          =>  $api_auth_port,
+        auth_protocol      =>  $api_auth_protocol,
+        admin_tenant_name  =>  $admin_tenant_name,
+        admin_user         =>  $admin_user,
+        admin_password     =>  $admin_password,
+	}
+	
+	class {'nozzle:worker':
+		listen						=>  $listen,
+		listen_port_range			=>  $listen_port_range,
+		configuration_backup_dir	=>	$configuration_backup_dir,
+	}
+	
+	class {
+
