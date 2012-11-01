df58000aec11c47c64555f3534f1046d4b506a9c
yuxcer <yuxcer@126.com>
2012-11-01 12:03:17 +0800

    move sql to server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,6 +1,19 @@
-class nozzle::server  {
-
- package {'nozzle-server':
+class nozzle::server(
+    $password,
+    $dbname = 'nozzle',
+    $user   = 'nozzle',
+    $host   = '127.0.0.1',
+    $sql    = '', 
+) {
+	package {'nozzle-server':
         ensure => present,
- }
+		require	=> Class['nozzle::mysql']
+   }
+    class {'nozzle::mysql':
+		password	=> $password,
+		dbname		=> $dbname,
+		user		=> $user,
+		host		=> $host,
+		sql			=> $sql,
+	}		
 }
--- manifests/worker.pp
+++ manifests/worker.pp
@@ -1,20 +1,6 @@
-class nozzle::worker(
-    $password,
-    $dbname = 'nozzle',
-    $user   = 'nozzle',
-    $host   = '127.0.0.1',
-    $sql    = '', 
-) {
+class nozzle::worker{
 	include 'nozzle::haproxy'
 	package {'nozzle-worker':
         ensure  => present,
-		require	=> Class['nozzle::mysql']
    }
-    class {'nozzle::mysql':
-		password	=> $password,
-		dbname		=> $dbname,
-		user		=> $user,
-		host		=> $host,
-		sql			=> $sql,
-	}		
 }
