b399745bf96cefa5f97297d616c0493a67b26d4c
yuxcer <yuxcer@126.com>
2012-11-08 11:27:23 +0800

    fix keystone.pp

--- manifests/keystone.pp
+++ manifests/keystone.pp
@@ -2,17 +2,20 @@ class nozzle::keystone (
     $email              = 'nozzle@sws.com',
     $enabled            = true,
     $ensure             = present,
-#    $id ,
     $nozzle_name		= 'nozzle',
     $password			= 'nozzle',
     $tenant             = 'services',
-    $public_address     = '127.0.0.1',                                                 
+    $public_address     = '127.0.0.1',       
     $admin_address      = '127.0.0.1',                                                 
     $internal_address   = '127.0.0.1', 
     $loadbalance_port    = '5556',
     $loadbalance_version = 'v1.0',
     $region             = 'RegionOne',
 ) {
+
+
+  Keystone_user_role["${nozzle_name}@services"] ~> Service <| name == 'nozzle-api' |>
+  Keystone_user_role["${nozzle_name}@services"] ~> Service <| name == 'nozzle-server' |>
 
     keystone_user {$nozzle_name:
        ensure   => $ensure,
@@ -24,13 +27,13 @@ class nozzle::keystone (
        roles   => 'admin', 
     }
 
-    keystone_service { $auth_name:                                                   
+    keystone_service { ${nozzle_name}:                                                   
         ensure      => present,                                                        
         type        => 'loadbalance', 
         description => "Openstack LoadBalance Service",                                 
     } 
 
-    keystone_endpoint { $auth_name:                                               
+    keystone_endpoint { ${nozzle_name}:                                               
         ensure       => present,                                                    
         region       => $region,                                                    
         public_url   => "http://${public_address}:${loadbalance_port}/${loadbalance_version}",  
