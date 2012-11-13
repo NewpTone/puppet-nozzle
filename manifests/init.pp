3aff98335d793218d281a4b287dc99abced203e6
yuxcer <yuxcer@126.com>
2012-11-13 16:26:50 +0800

    fix params error

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ class nozzle(
 		$listen								= '127.0.0.1',
 		$listen_port_range					= '10000,61000',
 		$haproxy_configuration_backup_dir   = '/var/lib/nozzle/backup/haproxy',
-		$nginx_configuration_backup_dir     = '/var/lib/nozzle/backup/nginx'
+		$nginx_configuration_backup_dir     = '/var/lib/nozzle/backup/nginx',
 		$tcp_postfixs						= '.elb4.sinasws.com',
 		$http_postfixs						= '.elb7.sinasws.com',
 ){
@@ -81,7 +81,7 @@ class nozzle(
 	config  =>  {
 		'listen'                    =>  $::listen,
 		'listen_port_range'         =>  $::listen_port_range,
-		'configuration_backup_dir'  =>  $::haproxy_configuration_backup_dir,
+		'configuration_backup_dir'  =>  $haproxy_configuration_backup_dir,
 	},  
 	order   => '02',
  }  
